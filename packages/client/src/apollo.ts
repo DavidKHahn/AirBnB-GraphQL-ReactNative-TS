43f3308855c10722abebadffaf19cb7018fbde36
David <dhahn123@gmail.com>
2019-10-18 15:22:35 -0700

    setup ApolloProvider, tslin.json in root

--- packages/client/src/apollo.ts
+++ packages/client/src/apollo.ts
@@ -1,11 +1,11 @@
-import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 
-const client = new ApolloClient({
+export const client = new ApolloClient({
   link: new HttpLink({
-      uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql',
-      credentials: 'same-origin'
+      uri: 'https://localhost:4000',
+      credentials: 'include'
     }),
   cache: new InMemoryCache()
 });
